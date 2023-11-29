f0ec1718d47633a3aed88cca07648959efb7bf0b
Kirill <K-baranov@outlook.com>
2023-11-29 17:42:41 +0100

    completed tasks from Clallenge Day24

---
+++ GuessTheFlag/FlagImageView.swift
@@ -0,0 +1,18 @@
+//
+//  ImageView.swift
+//  GuessTheFlag
+//
+//  Created by Kirill Baranov on 29/11/23.
+//
+
+import SwiftUI
+
+struct FlagImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FlagImageView()
+}
